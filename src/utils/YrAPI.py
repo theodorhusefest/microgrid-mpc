e682fac34db9b3fe8e94a457c9434a138d032052
Nicolai Hoel <nico2808@live.no>
2021-01-25 10:24:10 +0100

    Added an API for YR
    Simple API that gets air_temprature, wind_speed and wind_direction from YR. The location is Skien and is specified by the lat and long degrees in the URL.

---
+++ src/utils/YrAPI.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+#Hourly data for the first two days.
+#Two days into the future the predicitons are for each 6h intervall.
+response = requests.get("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.233033&lon=9.263709&altitude=14", headers={"User-Agent": "NicoAndTheo", "content-type": "text", }).text
+
+data = json.loads(response)
+temperature = []
+wind_speed = []
+wind_from_direction = []
+
+for x in data['properties']['timeseries']:
+    temperature.append(x['data']['instant']['details']['air_temperature'])
+    wind_speed.append(x['data']['instant']['details']['wind_speed'])
+    wind_from_direction.append(x['data']['instant']['details']['wind_from_direction'])
+
