9db00b7aaf924b8345516e3c523652de97052400
Nicolai Hoel <nico2808@live.no>
2021-01-26 10:18:41 +0100

    Solcast API connection
    This connection returns irradiance (W/m^2), air_temperature and end time for prediction intervall. To use it you need the API key.

---
+++ src/utils/SolcastAPI.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+#Need to insert API-key
+#30min prediction intervalls for one week ahead.
+def getSolcastData():
+    response = requests.get("https://api.solcast.com.au/weather_sites/0ae3-fb0a-96fc-c673/forecasts?format=json&api_key=API-NØKKEL-HER", headers={"User-Agent": "nicolho@stud.ntnu.no", "content-type": "text", }).text
+
+    data = json.loads(response)
+    GHI = []
+    end_time = []
+    temperature = []
+
+    for x in data['forecasts']:
+        GHI.append(x['ghi'])
+        end_time.append(x['period_end'])
+        temperature.append(x['air_temp'])
+
+    return GHI, temperature, end_time
